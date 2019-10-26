42691c2f224df32089618ad069fac09bfcc8c6fa
Kristoffer <troncosokristoffer@gmail.com>
2019-10-26 22:01:59 +0000

    Add highlighted icons based on current route on MobileNavbar
    When route matches, it highlights the icon for an app-like
    experience. Also changed icons from antd to react-icons.

--- src/components/MobileNavbar/MobileNavbar.js
+++ src/components/MobileNavbar/MobileNavbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Icon } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { FaPlusSquare, FaRegPlusSquare, FaUser, FaRegUser } from 'react-icons/fa';
+import { AiFillHome, AiOutlineHome } from 'react-icons/ai';
+import './MobileNavbar.css';
 
 function MobileNavbar({userData}) {
-
-  
+  const [currentPage, changeCurrentPage] = React.useState();
+  React.useEffect(() => {
+    console.log(currentPage)
+  }, [currentPage])
   return (
     <div 
       className="MobileNavbar"
@@ -21,9 +25,40 @@ function MobileNavbar({userData}) {
         alignItems: 'center'
       }}      
     >
-      <Link to="/"><Icon type="home" style={{fontSize: '28px', color: '#3b3b3b'}} /></Link>
-      <Link to="/post"><Icon type="plus-square" style={{fontSize: '38px', color: '#3b3b3b'}} /></Link>
-      <Link to={`/user/${userData.username}`}><Icon type="user" style={{fontSize: '28px', color: '#3b3b3b'}} /></Link>
+      <NavLink 
+        exact
+        to="/"
+        isActive={(match, location) => {
+          match ? changeCurrentPage('home') : console.log('yo')
+        }}
+      >
+        {currentPage === 'home'
+          ? <AiFillHome style={{fontSize: '34px', color: '#3b3b3b'}} />
+          : <AiOutlineHome style={{fontSize: '34px', color: '#3b3b3b'}} />
+        }
+      </NavLink>
+      <NavLink 
+        to="/post" 
+        isActive={(match, location) => {
+          match ? changeCurrentPage('post') : console.log('yo')
+        }}
+      >
+        {currentPage === 'post'
+          ? <FaPlusSquare style={{fontSize: '38px', color: '#3b3b3b'}} />
+          : <FaRegPlusSquare style={{fontSize: '38px', color: '#3b3b3b'}} />
+        }
+      </NavLink>
+      <NavLink 
+        to={`/user/${userData.username}`}
+        isActive={(match, location) => {
+          match ? changeCurrentPage('user') : console.log('yo')
+        }}        
+      >
+        {currentPage === 'user'
+          ? <FaUser style={{fontSize: '30px', color: '#3b3b3b'}} />
+          : <FaRegUser style={{fontSize: '30px', color: '#3b3b3b'}} />
+        }
+      </NavLink>
     </div>
   )
 }
