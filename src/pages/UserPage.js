8bdb34587dcb3b296ad76217520403900051c126
Kristoffer <troncosokristoffer@gmail.com>
2019-10-30 14:09:44 +0000

    Fix handling of unknown user

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -6,7 +6,8 @@ import { API, graphqlOperation } from 'aws-amplify';
 
 function UserPage({loggedInUserData, props}) {
   const [foundUserData, changeFoundUserData] = React.useState({posts: []})
-
+  const [isFound, changeIsFound] = React.useState();
+  
   React.useEffect(() => {
     console.log(`UserPage-useEffect: Getting user data for this page!`);
     getUser(props.match.params.id);
@@ -60,23 +61,34 @@ function UserPage({loggedInUserData, props}) {
     };
     
     let res = await API.graphql(graphqlOperation(customListUsers, variables));
-    let {id, name, username, bio, email, photoUrl, userPosts} = res.data.listUsers.items[0];
-    changeFoundUserData({
-      id: id,
-      name: name,
-      username: username,
-      bio: bio,
-      profilePhotoUrl: photoUrl,
-      email: email,
-      posts: userPosts.items
-    })
+    console.log(res);
+    if (res.data.listUsers.items.length > 0) {
+      changeIsFound(true);
+      let {id, name, username, bio, email, photoUrl, userPosts} = res.data.listUsers.items[0];
+      changeFoundUserData({
+        id: id,
+        name: name,
+        username: username,
+        bio: bio,
+        profilePhotoUrl: photoUrl,
+        email: email,
+        posts: userPosts.items
+      })     
+    } else {
+      console.log("User does not exist");
+      changeIsFound(false);
+    }
   }
   
   
   return (
     <div style={{padding: "20px 0"}}>
-      <InfoHeader userData={foundUserData} loggedInUserData={loggedInUserData} />
-      <PicGrid pics={foundUserData.posts} userData={foundUserData} loggedInUserData={loggedInUserData} getUser={getUser} />
+      {isFound 
+      ? <>
+          <InfoHeader userData={foundUserData} loggedInUserData={loggedInUserData} />
+          <PicGrid pics={foundUserData.posts} userData={foundUserData} loggedInUserData={loggedInUserData} getUser={getUser} />
+        </> 
+      : <h1 style={{textAlign: 'center', paddingTop: '200px'}}>User not found</h1>}
     </div>
   )
 }
