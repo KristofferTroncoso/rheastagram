87cea53e1b1a124848411d7b03390f456eb1224b
Kristoffer <troncosokristoffer@gmail.com>
2019-11-08 18:46:17 +0000

    Add rainbow and large api to Avatar component and redo styling

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,25 +4,46 @@ import { Avatar as AntAvatar } from 'antd';
 import { Storage } from 'aws-amplify';
 import styled from 'styled-components';
 
+// const StyledDiv = styled.div`
+//   background: #ec008c; 
+//   background: -webkit-linear-gradient(to right, #ffc852, #ec008c); 
+//   background: linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+//             linear-gradient(127deg, rgba(255,255,0,.8), rgba(255,255,0,0) 70.71%),
+//             linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);
+//   padding: 2px;
+//   height: 44px;
+//   width: 44px;
+//   border-radius: 50%;
+// `;
+
+// const StyledAntAvatar = styled(AntAvatar)`
+//   width: 40px;
+//   height: 40px;
+//   border: 2px solid white;
+// `;
+
 const StyledDiv = styled.div`
-  background: #ec008c; 
-  background: -webkit-linear-gradient(to right, #ffc852, #ec008c); 
-  background: linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
-            linear-gradient(127deg, rgba(255,255,0,.8), rgba(255,255,0,0) 70.71%),
-            linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);
-  padding: 2px;
-  height: 44px;
-  width: 44px;
+  ${props => props.rainbow
+    ? `background: #ec008c; 
+      background: -webkit-linear-gradient(to right, #ffc852, #ec008c); 
+      background: linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+              linear-gradient(127deg, rgba(255,255,0,.8), rgba(255,255,0,0) 70.71%),
+              linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);`
+    : `background: none;`
+  }
+  padding: ${props => props.rainbow ? "2px" : "0"};
   border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
 `;
 
 const StyledAntAvatar = styled(AntAvatar)`
-  width: 40px;
-  height: 40px;
   border: 2px solid white;
 `;
 
-function Avatar({img, username}) {
+function Avatar({img, username, large, rainbow}) {
   const [imgKey, changeImgKey] = React.useState();
   
   React.useEffect(() => {
@@ -32,16 +53,16 @@ function Avatar({img, username}) {
   
   return (
     <Link to={`/user/${username}`}>
-      <StyledDiv className="AvatarIcon">
+      <StyledDiv className="AvatarIcon" rainbow={rainbow}>
         <StyledAntAvatar 
           icon="user" 
           src={imgKey && imgKey} 
+          size={large ? "large" : "default"}
         />
       </StyledDiv>
     </Link>
   )
 }
-
 
 
 export default Avatar;
