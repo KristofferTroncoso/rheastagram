af9e3ad714060a08d606dceec603ca4e7f350109
Kristoffer <troncosokristoffer@gmail.com>
2019-11-19 15:02:51 +0000

    Refactor InfoHeader styling

--- src/components/InfoHeader/InfoHeader.js
+++ src/components/InfoHeader/InfoHeader.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './InfoHeader.css';
 import { Link } from 'react-router-dom';
 import { Storage, Cache } from 'aws-amplify';
 import SettingsModal from '../SettingsModal/SettingsModal';
+import styled from 'styled-components/macro';
 
 
 function InfoHeader({userData, loggedInUserData}) {
@@ -25,13 +25,80 @@ function InfoHeader({userData, loggedInUserData}) {
   }, [userData.profilePhotoUrl])
   
   return (
-    <div className="InfoHeader wrapper" style={{marginBottom: '30px'}}>
-      <div className="InfoHeader_img_container">
-      {userData.profilePhotoUrl ? <img className="InfoHeader_img" alt={imgKey} src={imgKey} /> : null}
+    <div 
+      className="InfoHeader wrapper"
+      css={`
+        height: 200px;
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        margin: 100px auto 10px;
+        max-width: 800px;
+        margin-bottom: 30px;
+        
+        @media (max-width: 768px){ 
+          margin: 60px auto 10px;
+          height: 140px;
+          max-width: 600px;
+          padding: 4px;
+        }
+      `}
+    >
+      <div 
+        className="InfoHeader_img_container"
+        css={`
+          background: lightgrey;
+          clip-path: circle(77px at center);
+          height: 100%;
+          
+          @media (max-width: 768px){ 
+              height: 70px;
+              clip-path: circle(36px at center);
+          }
+        `}
+      >
+        {userData.profilePhotoUrl 
+        ? <img 
+            className="InfoHeader_img" 
+            alt={imgKey} 
+            src={imgKey} 
+            css={`
+              height: 100%;
+              clip-path: circle(76px at center);
+              
+              @media (max-width: 768px){ 
+                height: 70px;
+                clip-path: circle(35px at center);
+              }
+            `}
+          /> 
+        : null
+        }
       </div>
       <div>
-        <div className="InfoHeader_username_container">
-          <h1 className="InfoHeader_username" style={{marginBottom: '5px'}}>{userData.username}</h1>
+        <div 
+          className="InfoHeader_username_container"
+          css={`
+            display: flex;
+            align-content: center;
+            align-items: center;
+            margin-bottom: 14px;
+            
+            @media (max-width: 768px){ 
+              margin-bottom: 0px;
+            }
+          `}
+        >
+          <h1 
+            className="InfoHeader_username" 
+            css={`
+              font-weight: 300;
+              font-size: 34px;
+              margin-bottom: 5px;
+            `}
+          >
+            {userData.username}
+          </h1>
           {userData.username === loggedInUserData.username 
             ? <>
                 <Link to="/editprofile">
