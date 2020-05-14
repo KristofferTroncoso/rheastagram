c3c6694a69d798afcec105d3936fe9656e7eeb0d
KristofferTroncoso <troncosokristoffer@gmail.com>
2020-05-14 03:27:38 -0400

    Fix bottom margin/padding issue in mobile

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -4,12 +4,14 @@ import { css, jsx } from '@emotion/core';
 function Wrapper({children}) {
   return (
     <div 
+      className="Wrapper"
       css={css`
         max-width: 1000px; 
         margin: 80px auto;
 
         @media (max-width: 768px){ 
-          margin: 60px auto;
+          margin: 60px auto 0;
+          padding-bottom: 50px;
         }
       `}
     >
