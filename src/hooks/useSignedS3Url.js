b86ec0df4db2bfa4e950ec99273401f5543676ce
KristofferTroncoso <troncosokristoffer@gmail.com>
2020-06-20 17:07:34 -0400

    Temp fix: Bypass image cache
    Temporarily bypassing image cache for 24 hours due to expired
    signed urls being used.

--- src/hooks/useSignedS3Url.js
+++ src/hooks/useSignedS3Url.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import { Storage, Cache } from 'aws-amplify';
 
+// hoursToCacheImageInBrowser is located in SubmitPostPage.js
+// how long the signed url is valid for
+let hoursTillSignedUrlExpires = 12
+// hoursToCacheSignedUrl should be less than or equal to hoursTillSignedUrlExpires
+let hoursToCacheSignedUrl = 11;
+
 function useSignedS3Url(imgKey) {
   const [signedUrl, setSignedUrl] = React.useState();
   
+  // React.useEffect(() => {
+  //   if (!imgKey) {
+  //     console.log("image key is undefined");
+  //     setSignedUrl(undefined);
+  //   } else {
+  //     let cacheRes = Cache.getItem(imgKey);
+  //     if (cacheRes === null) {
+  //       console.log('getting new signed url for image')
+  //       Storage.get(imgKey, { expires: 3600 * hoursTillSignedUrlExpires })
+  //       .then(data => {
+  //         setSignedUrl(data);
+  //         const dateNow = new Date();
+  //         const expirationTime = dateNow.getTime() + (3600000 * hoursToCacheSignedUrl);
+  //         Cache.setItem(imgKey, data, {expires: expirationTime });
+  //       })
+  //       .catch(err => console.log(err));
+  //     } else {
+  //       console.log("Image is in cache (You saved bandwidth!)")
+  //       setSignedUrl(cacheRes);
+  //     }
+  //   }
+  // }, [imgKey]);
+
   React.useEffect(() => {
     if (!imgKey) {
       console.log("image key is undefined");
@@ -12,22 +41,27 @@ function useSignedS3Url(imgKey) {
       let cacheRes = Cache.getItem(imgKey);
       if (cacheRes === null) {
         console.log('getting new signed url for image')
-        Storage.get(imgKey)
+        Storage.get(imgKey, { expires: 3600 * hoursTillSignedUrlExpires })
         .then(data => {
           setSignedUrl(data);
-          // hoursToCacheSignedUrl should be less than or equal to how long the image is cached in the browser
-          let hoursToCacheSignedUrl = 0.2;
           const dateNow = new Date();
           const expirationTime = dateNow.getTime() + (3600000 * hoursToCacheSignedUrl);
           Cache.setItem(imgKey, data, {expires: expirationTime });
         })
         .catch(err => console.log(err));
-      } else {
-        console.log("Image is in cache (You saved bandwidth!)")
-        setSignedUrl(cacheRes);
+      } else {   
+        Storage.get(imgKey, { expires: 3600 * hoursTillSignedUrlExpires })
+        .then(data => {
+          setSignedUrl(data);
+          const dateNow = new Date();
+          const expirationTime = dateNow.getTime() + (3600000 * hoursToCacheSignedUrl);
+          Cache.setItem(imgKey, data, {expires: expirationTime });
+        })
+        .catch(err => console.log(err));
       }
     }
   }, [imgKey]);
+
   
   return signedUrl;
 }
