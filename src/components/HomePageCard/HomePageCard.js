52ee657e19bbb3ede4fb5cc616b363f724df5382
KristofferTroncoso <troncosokristoffer@gmail.com>
2020-06-25 19:17:17 -0400

    Update HomePageCard with more info and functionality

--- src/components/HomePageCard/HomePageCard.js
+++ src/components/HomePageCard/HomePageCard.js
@@ -1,13 +1,18 @@
 /** @jsx jsx */
 import React from 'react';
 import Avatar from '../Avatar/Avatar';
-import { MessageOutlined, UploadOutlined, HeartOutlined } from '@ant-design/icons';
+import { API } from 'aws-amplify';
+import { MessageOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import PostOptions from '../PostOptions/PostOptions';
-import { jsx } from '@emotion/core';
+import { css, jsx } from '@emotion/core';
 import styled from '@emotion/styled';
 import useSignedS3Url from '../../hooks/useSignedS3Url';
 import UsernameLink from '../UsernameLink/UsernameLink';
+import Like from '../Like/Like';
+import { LoggedInUserContext } from '../../user-context';
+import { Popover } from 'antd';
+import CommentForm from '../CommentForm/CommentForm';
 
 const StyledSection = styled.section`
   background: white;
@@ -34,78 +39,171 @@ const StyledDiv = styled.div`
   align-content: center;
 `;
 
-const StyledTimestamp = styled.p`
-  color: grey;
-  font-size: 11px;
-  margin: 12px 10px 6px;
-`;
-
-const StyledHeartOutlined = styled(HeartOutlined)`
-  font-size: 24px;
-  margin: 0 8px;
-  color: #5c5c5c;
-`;
-
-const StyledMessageOutlined = styled(MessageOutlined)`
-  font-size: 24px;
-  margin: 0 8px;
-  color: #5c5c5c;
-`;
-
-const StyledUploadOutlined = styled(UploadOutlined)`
-  font-size: 24px;
-  margin: 0 8px;
-  color: #5c5c5c;
-`;
 
 const StyledImg = styled.img`
   width: 100%;
   min-height: 200px;
-  background: #3A1C71;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #FFAF7B, #D76D77, #3A1C71); 
-  background: linear-gradient(to right, #FFAF7B, #D76D77, #3A1C71); 
+  background: #e6e6e6;
   filter: ${props => props.isImgLoaded ? 'none' : 'blur(10px)'};
   border-top: 1px solid #e6e6e6;
   border-bottom: 1px solid #e6e6e6;
 `;
 
-function HomePageCard({ id, imgUrl, likes, hearts, userData, createdAt }) {
-  const imgKey = useSignedS3Url(imgUrl);
+function HomePageCard({ postId }) {
+  const { isAuthenticated } = React.useContext(LoggedInUserContext);
   const [isImgLoaded, setIsImgLoaded] = React.useState(false);
+  const [postData, changePostData] = React.useState({
+    id: '',
+    picUrl: '',
+    type: '',
+    visibility: '',
+    timeCreated: '',
+    userId: '',
+    user: {},
+    comments: {items: []},
+    likes: {items: [{user: {id: '', photoUrl: '', username: ''}}]}
+  });
+  const imgKey = useSignedS3Url(postData.picUrl);
 
-  const handleLike = e => {
-    console.log(`Liking post ${id}`);
+  const getPostData = async (postId) => {
+    const query = `
+      query GetPost($postId: ID!, $commentsLimit: Int) {
+        getPost(id: $postId) {
+          id
+          picUrl
+          type
+          visibility
+          timeCreated
+          userId
+          user {
+            id
+            username
+            photoUrl
+          }
+          comments(limit: $commentsLimit) {
+            items {
+              id
+              content
+              timeCreated
+              user {
+                id
+                photoUrl
+                username
+              }
+            }
+          }
+          likes {
+            items {
+              id
+              user {
+                id
+                username
+                photoUrl
+              }
+            }
+          }
+        }
+      }
+    `;
+    
+    const variables = {
+      postId: postId,
+      commentsLimit: 3
+    }
+    
+    API.graphql({query, variables})
+    .then(res => {
+      changePostData(res.data.getPost);
+    })
+    .catch(err => console.log(err));
   }
+
+  React.useEffect(() => {
+    getPostData(postId);
+  }, [postId]);
   
   return (
     <StyledSection className="wrapper">
       <StyledDiv>
         <div style={{display: 'flex', alignItems: 'center'}}>
           <div css={{marginRight: '10px'}}>
-            <Avatar img={userData.photoUrl}  username={userData.username} rainbow large />
+            <Avatar img={postData.user.photoUrl}  username={postData.user.username} rainbow large />
           </div>
-          <UsernameLink>{userData.username}</UsernameLink>
+          <UsernameLink>{postData.user.username}</UsernameLink>
         </div>
         <PostOptions 
-          userDataId={userData.id} 
-          postId={id}
+          userDataId={postData.user.id} 
+          postId={postData.id}
         />
       </StyledDiv>
       <div style={{overflow: 'hidden'}}>
         <StyledImg 
           src={imgKey} 
-          alt={isImgLoaded ? imgUrl : null} 
+          alt={isImgLoaded ? imgKey : null} 
           onLoad={e => setIsImgLoaded(true)} 
           isImgLoaded={isImgLoaded}
         />
       </div>
-      <div style={{padding: '15px'}}>
-        <button onClick={handleLike} style={{border: 0, padding: 0, outline: 0}}>
-          <StyledHeartOutlined />
-        </button>
-        <StyledMessageOutlined />
-        <StyledUploadOutlined />
-        <StyledTimestamp>{moment(createdAt).fromNow().toUpperCase()}</StyledTimestamp>
+      <div>
+        <div className="PostCard_stats_icons" css={css`display: flex; padding: 10px 10px 2px`}>
+          <Like postId={postId} getPostData={getPostData} />
+          <MessageOutlined 
+            css={css`font-size: 24px; margin: 0 8px; color: #5c5c5c;`}
+            onClick={isAuthenticated ? e => document.getElementById(`CommentForm_input_${postId}`).focus() : null}
+          />
+        </div>
+        {postData.likes.items.length > 0 &&
+          <div css={css`display: flex; align-content: center; align-items: center; padding: 2px 10px`}>
+            <div css={css`margin-right: 5px`}><Avatar img={postData.likes.items[0].user.photoUrl} username={postData.likes.items[0].user.username} /></div>
+            <span>
+              Liked by <UsernameLink>{postData.likes.items[0].user.username}</UsernameLink>
+              {postData.likes.items.length > 1 &&
+                <span>
+                  and 
+                  <Popover 
+                    trigger="click"
+                    content={
+                      <div>
+                        {postData.likes.items.slice(1).map(item => (
+                          <span key={item.id}><UsernameLink>{item.user.username}</UsernameLink></span>
+                        ))}
+                      </div>
+                    }
+                  >
+                    <span css={{fontWeight: '600', color: 'black', marginLeft: '4px', cursor: 'pointer'}}>
+                      {postData.likes.items.length - 1} {postData.likes.items.length === 2 ? 'other' : 'others'}
+                    </span>
+                  </Popover>
+                </span>
+              }
+            </span>
+          </div>
+        }
+        <div 
+          className="CommentList" 
+          css={css`padding: 2px 10px; height: 100%; overflow: auto;`}
+        >
+          {postData.comments.items.map(comment => (
+            <div css={css`display: flex; align-items: baseline`}>
+              <UsernameLink>{comment.user.username}</UsernameLink>
+              <p css={css`font-size: 14px; color: #2b2b2b; margin: 0`}>
+                {comment.content}
+              </p>
+            </div>
+          ))}
+        </div> 
+        <span css={css`color: grey; font-size: 12px; padding: 10px;`}>
+          {moment(postData.timeCreated).format('MMMM D, YYYY')}
+        </span>
+        <div 
+          css={css`
+            width: 100%; 
+            border-top: 1px solid #efefef;
+            padding: 0 2px 0 10px;
+          `}
+        >
+          <CommentForm postId={postId} getPostData={getPostData} /> 
+        </div>
       </div>
     </StyledSection>
   )
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,7 +4,6 @@ import { API, graphqlOperation } from 'aws-amplify';
 import HomePageCard from '../../components/HomePageCard/HomePageCard';
 import { css, jsx } from '@emotion/core';
 
-
 const HomePage = () => {
   React.useEffect(() => {
     getAllPosts();
@@ -14,50 +13,34 @@ const HomePage = () => {
 
   const getAllPosts = async () => {
     const query = `
-      query ListPosts(
-        $filter: ModelPostFilterInput
-        $limit: Int
-        $nextToken: String
+      query ListPostsByVisibility(
+        $visibility:String
+        $sortDirection:ModelSortDirection
+        $limit:Int
+        $nextToken:String
       ) {
-        listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+        listPostsByVisibility(
+          visibility:$visibility
+          sortDirection: $sortDirection
+          limit: $limit
+          nextToken: $nextToken
+        ) {
           items {
             id
-            picUrl
-            user {
-              id
-              username
-              name
-              bio
-              email
-              photoUrl
-            }
-            comments {
-              nextToken
-            }
-            likes {
-              nextToken
-            }
-            timeCreated
           }
-          nextToken
         }
       }
     `;
 
     const variables = {
-      limit: 12
+      visibility: 'public',
+      sortDirection: 'DESC',
+      limit: 12,
+      nextToken: null
     }
 
     let response = await API.graphql(graphqlOperation(query, variables));
-    let sortedPosts = response.data.listPosts.items.sort((a, b) => (
-      (a.timeCreated < b.timeCreated) 
-      ? -1 
-      : ((a.timeCreated > b.timeCreated) 
-        ? 1 
-        : 0)
-      )
-    ).reverse();
-    changeAllPosts(sortedPosts);
+    changeAllPosts(response.data.listPostsByVisibility.items);
   }
 
   return (
@@ -69,10 +52,7 @@ const HomePage = () => {
       {allPosts.map(post => (
         <HomePageCard
           key={post.id}
-          id={post.id}
-          imgUrl={post.picUrl}
-          createdAt={post.timeCreated}
-          userData={post.user}
+          postId={post.id}
         />
       ))}
     </div>
